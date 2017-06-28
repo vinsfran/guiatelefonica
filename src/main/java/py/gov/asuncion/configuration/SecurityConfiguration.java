bf8bf44b91122c164c10735c158885cd7214646a
Vicente Insfran <vinsfran@gmail.com>
2017-06-28 17:24:12 -0400

    Modificacion en tables

--- src/main/java/py/gov/asuncion/configuration/SecurityConfiguration.java
+++ src/main/java/py/gov/asuncion/configuration/SecurityConfiguration.java
@@ -43,7 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/css/*", "/fonts/*", "/img/*", "/js/*").permitAll()
+                .antMatchers("/css/*", "/fonts/*", "/img/*", "/js/*", "/tables/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").loginProcessingUrl("/logincheck")
