e41dfee6f71d4c25748efe8c6d1236d3700d6690
Vicente Insfran <vinsfran@gmail.com>
2017-06-30 13:35:46 -0400

    Correcciones form de Tipos de Lugares

--- src/main/java/py/gov/asuncion/controller/AdminController.java
+++ src/main/java/py/gov/asuncion/controller/AdminController.java
@@ -1,24 +1,18 @@
 package py.gov.asuncion.controller;
 
-import py.gov.asuncion.constant.ViewConstant;
-import py.gov.asuncion.model.ContactModel;
-import py.gov.asuncion.service.ContactService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+import py.gov.asuncion.constant.ViewConstant;
 import py.gov.asuncion.service.TipoLugarService;
 
 /**
@@ -57,6 +51,11 @@ public class AdminController {
     @GetMapping("/tables")
     public String showTables(Model model) {
         return TABLES_VIEW;
+    }
+    
+    @GetMapping("/tiposlugares")
+    public RedirectView redirect() {
+        return new RedirectView("tipolugar/list");
     }
 
 //    @GetMapping("/tipolugar")
